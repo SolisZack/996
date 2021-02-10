fd834c6047599c7dbbf6baf44f8a4423426e0993
wwd <597235966@qq.com>
2021-02-10 22:54:05 +0800

    test test~

--- test.go
+++ test.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	var a float32 = 3.0
 	var b int = 3
+	c := "sad"
 
-	fmt.Println(reflect.TypeOf(a), reflect.TypeOf(b))
+	fmt.Println(reflect.TypeOf(a), reflect.TypeOf(b), c)
 }
